9ddcd0bea8dcc3820ecc42af0116507f185a8818
Andrew Welch <andrew@nystudio107.com>
2021-05-08 01:09:06 -0400

    Updated docs config
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- docs/docs/.vitepress/config.js
+++ docs/docs/.vitepress/config.js
@@ -4,5 +4,14 @@ module.exports = {
     base: '/docs/vite/',
     lang: 'en-US',
     themeConfig: {
-    }
+        repo: 'nystudio107/craft-vite',
+        docsDir: 'docs',
+        algolia: {
+            apiKey: 'c57105e511faa5558547599f120ceeba',
+            indexName: 'vitepress'
+        },
+        editLinks: true,
+        editLinkText: 'Edit this page on GitHub',
+        lastUpdated: 'Last Updated',
+    },
 };
